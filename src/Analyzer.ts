f177babdc083860f8881fbea799648cbf91c4331
Sander van Vliet <sander@codenizer.nl>
2016-03-21 07:03:42 +0100

    Move stuff over from sonar-typescript and get the deps installed

---
+++ src/Analyzer.ts
@@ -0,0 +1,49 @@
+/// <reference path="../typings/main.d.ts" />
+/// <reference path="../node_modules/typescript/lib/typescript.d.ts" />
+
+import {readFileSync} from "fs";
+import * as ts from "typescript";
+import d = require("./Domain");
+
+module SonarTypescript {
+    export function analyzeFile(sourceFile: ts.SourceFile) {
+        var fileMetrics = new d.Domain.FileMetric();
+
+        console.log(sourceFile);
+        analyzeNode(sourceFile);
+
+        function analyzeNode(node: ts.Node) {
+            console.log("node kind: " + ts.SyntaxKind[node.kind]);
+
+            switch (node.kind) {
+                case ts.SyntaxKind.ModuleDeclaration:
+                    fileMetrics.NumberOfModules++;
+                    break;
+                case ts.SyntaxKind.InterfaceDeclaration:
+                    fileMetrics.NumberOfInterfaces++;
+                    break;
+                case ts.SyntaxKind.ClassDeclaration:
+                    fileMetrics.NumberOfClasses++;
+                    break;
+                case ts.SyntaxKind.MethodDeclaration:
+                    fileMetrics.NumberOfMethods++;
+                    break;
+                case ts.SyntaxKind.EndOfFileToken:
+                    var lineAndChar = sourceFile.getLineAndCharacterOfPosition(node.end);
+                    fileMetrics.NumberOfLines = lineAndChar.line + 1;
+                    break;
+                default:
+                    break;
+            }
+
+            ts.forEachChild(node, analyzeNode);
+        }
+        console.log("File: " + sourceFile.fileName);
+        console.log(fileMetrics);
+
+        function report(node: ts.Node, message: string) {
+            let { line, character } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
+            console.log(`${sourceFile.fileName} (${line + 1},${character + 1}): ${message}`);
+        }
+    }
+}
---
+++ src/Domain.ts
@@ -0,0 +1,9 @@
+export module Domain {
+    export class FileMetric {
+        NumberOfModules: number = 0;
+        NumberOfInterfaces: number = 0;
+        NumberOfClasses: number = 0;
+        NumberOfMethods: number = 0;
+        NumberOfLines: number = 0;
+    };
+}
