ec8ef07e45c2c8bcfce0051b50b8ce59c25ee07e
Sander van Vliet <sander@codenizer.nl>
2016-04-24 16:16:55 +0200

    Use scanner instead of parser to analyze typescript files

--- src/Analyzer.ts
+++ src/Analyzer.ts
@@ -8,43 +8,71 @@ import o = require("./JsonOutput");
 
 export module SonarTypeScript {
     export class Analyzer {
+        private scanner = ts.createScanner(ts.ScriptTarget.ES5, /*skipTrivia*/ false);
+
         constructor(private outputter: o.Output.IFileMetricOutput) {
         }
-        
+
         public analyzeFile(sourceFile: ts.SourceFile) {
             var fileMetrics = new d.Domain.FileMetric();
             fileMetrics.FileName = sourceFile.fileName;
 
-            this.analyzeNode(sourceFile, fileMetrics, sourceFile);
-            
+            this.initializeState(sourceFile.getFullText());
+            this.scanFile(sourceFile, fileMetrics);
+
             this.outputter.writeMetric(fileMetrics);
         }
-                    
-        private analyzeNode(node: ts.Node, fileMetrics: d.Domain.FileMetric, sourceFile: ts.SourceFile) {
-                switch (node.kind) {
-                    case ts.SyntaxKind.ModuleDeclaration:
-                        fileMetrics.NumberOfModules++;
-                        break;
-                    case ts.SyntaxKind.InterfaceDeclaration:
-                        fileMetrics.NumberOfInterfaces++;
-                        break;
-                    case ts.SyntaxKind.ClassDeclaration:
-                        fileMetrics.NumberOfClasses++;
-                        break;
-                    case ts.SyntaxKind.MethodDeclaration:
-                    case ts.SyntaxKind.FunctionDeclaration:
-                        fileMetrics.NumberOfMethods++;
-                        break;
-                    case ts.SyntaxKind.EndOfFileToken:
-                        var lineAndChar = sourceFile.getLineAndCharacterOfPosition(node.end);
-                        fileMetrics.NumberOfLines = lineAndChar.line + 1;
-                        fileMetrics.LinesOfCode = fileMetrics.NumberOfLines;
-                        break;
-                    default:
-                        break;
-                }
 
-                ts.forEachChild(node, n => this.analyzeNode(n, fileMetrics, sourceFile));
+        private scanFile(sourceFile: ts.SourceFile, metrics: d.Domain.FileMetric) {
+            var token = this.scanner.scan();
+            while (token != ts.SyntaxKind.EndOfFileToken) {
+                this.updateMetricFor(<ts.SyntaxKind>token, metrics);
+                console.log(this.scanner.getTokenText());
+                token = this.scanner.scan();
             }
+        }
+
+        private initializeState(text: string): void {
+            this.scanner.setText(text);
+            this.scanner.setOnError((message: ts.DiagnosticMessage, length: number) => {
+                console.error(message);
+            });
+            this.scanner.setScriptTarget(ts.ScriptTarget.ES5);
+            this.scanner.setLanguageVariant(ts.LanguageVariant.Standard);
+        }
+
+        private prevTokenKind: ts.SyntaxKind;
+
+        private updateMetricFor(kind: ts.SyntaxKind, fileMetrics: d.Domain.FileMetric) {
+            console.log("kind: " + ts.SyntaxKind[kind]);
+            switch (kind) {
+                case ts.SyntaxKind.ModuleDeclaration:
+                    fileMetrics.NumberOfModules++;
+                    break;
+                case ts.SyntaxKind.InterfaceDeclaration:
+                    fileMetrics.NumberOfInterfaces++;
+                    break;
+                case ts.SyntaxKind.ClassDeclaration:
+                    fileMetrics.NumberOfClasses++;
+                    break;
+                case ts.SyntaxKind.MethodDeclaration:
+                case ts.SyntaxKind.FunctionDeclaration:
+                    fileMetrics.NumberOfMethods++;
+                    break;
+                case ts.SyntaxKind.NewLineTrivia:
+                    fileMetrics.NumberOfLines++;
+                    fileMetrics.LinesOfCode++;
+             
+                    if (this.prevTokenKind != null && this.prevTokenKind == ts.SyntaxKind.NewLineTrivia) {
+                        fileMetrics.LinesOfCode--;
+                    }
+                    
+                    break;
+                default:
+                    break;
+            }
+
+            this.prevTokenKind = kind;
+        }
     }
 }
